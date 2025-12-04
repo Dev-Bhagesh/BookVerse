f0ff035fb3be5288fea42b67c8324e3dc3ce4974
Bhagesh <bhageshniloor159@gmail.com>
2025-12-04 07:36:34 +0530

    I added ejs file in this i didnt did much i am learning multer and ejs and express-session for next features of this project

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const path = require('path');
 
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/fullstack-auth", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+
+
+
 
 // Middleware
+app.set('view engine','ejs')
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cors());
 
@@ -28,13 +40,15 @@ app.get("/gener",(req,res)=>{
     res.sendFile(path.join(__dirname,'views','gener.html'));
 })
 
-// Connect MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/fullstack-auth", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+app.get('/myprofile',(req,res)=>{
+    res.render('myprofile',{
+        profilePic : null,
+        username : 'guest account',
+        bio : "I am Bhagesh and nice to meet you",
+        posts:[]
+    })
 })
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+
 
 
 app.listen(5000, () => console.log("Server running on port 5000"));
